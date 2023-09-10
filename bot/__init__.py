743e3e7bdf091c4c127195ba20931a542c90bb83
Hirokiro21 <119547580+Hirokiro21@users.noreply.github.com>
2023-09-10 14:25:17 +0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -551,7 +551,7 @@ sleep(0.5)
 if ospath.exists('accounts.zip'):
     if ospath.exists('accounts'):
         srun(["rm", "-rf", "accounts"])
-    srun(["7z", "x", "accounts.zip", "-oaccounts", "-aoa", "*.json"])
+    srun(["7z", "x", "-o.", "-aoa", "accounts.zip", "accounts/*.json"])
     srun(["chmod", "-R", "777", "accounts"])
     osremove('accounts.zip')
 if not ospath.exists('accounts'):
